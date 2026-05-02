57f690b9a11e4ad2860e4672feec274a07fdbb09
jideatom <95307473+jideatom@users.noreply.github.com>
2026-05-02 20:05:52 +0100

    Build Vite app entry separately

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/LifeOS/',
+  build: {
+    rollupOptions: {
+      input: 'app.html',
+    },
+  },
   plugins: [react()],
 })
